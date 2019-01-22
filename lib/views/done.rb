99b3e682367fe51455aca5b8a2390f4115cf5d4c
TheWalkingNev <alexandre.gasior@gmail.com>
2019-01-22 22:48:12 +0100

    Harry Powwaaa git status

---
+++ lib/views/done.rb
@@ -0,0 +1,109 @@
+def harry
+  sleep(4)
+  puts `clear`
+  puts "\n\n\n\n\n\n\n\n\n\n                                                   ######################################################"
+  puts "                                                    ##### ~~          MEFAIT ACCOMMPLI !        ~~ #####"
+  puts "                                                     --------------------------------------------------\n\n"
+  sleep(2)
+  puts "````````````````````````````````````````.-+hyyhyyhyhmmdmmmmmdyssyhhhdmmmmmmNNmdhmNdhs.`````````````````````.``````...``...:+o........................."
+  sleep(0.05)
+  puts "````````````````````````````````````````.:hhddhhhhdmmmdmNNNmdhyhhddmmmNmmmmmmNNNddNNh:````````````````````````````........:sh........................."
+  sleep(0.05)
+  puts "```````````````````````````````````````./ydddhhhddhhhhyhmmNNmmmddmddmNNmmmhdmmNNNNmmNh.````````````````````````````.......-hy:........................"
+  sleep(0.05)
+  puts "```````````````````````````````````````.:dddddddddhyo+/+syyhhyyyyhdmdmmNNNNNmNNNNNNmmm:``````````````````````````````````..yso`......................."
+  sleep(0.05)
+  puts "````````````````````````````````````````:ddmmddmmo/:----:::+++sydmmdNNNNNNMMMMMMNNNNMm+````````````````````````````````````+sh.``....................."
+  sleep(0.05)
+  puts "````````````````````````````````````````.ydmdhhNh/::-------:::/ohdmmdmNNMNMMMMMMMNNNNNo````````````````````````````````````:yy/```...................."
+  sleep(0.05)
+  puts "````````````````````````````````````````.osddhyms/::----.----.-:sshddydNNmmddmNNNNNNNN/````````````````````````````````````.yso``....................."
+  sleep(0.05)
+  puts "````````````````````````````````````````.+dhhhsyo//:/:-------..-++oyhhyyyyso/+sdNNNNNm-`````````````````````````````````````soh``....................."
+  sleep(0.05)
+  puts "`````````````````````````````````````````-ddddsss++sssssss+/:--:+osysoo+++++//o+hNNNNh``````````````````````````````````````ooh.``...................."
+  sleep(0.05)
+  puts "``````````````````````````````````````````/hdNNdyso++oosssso/:-:/ooo/yhysso/+///ohNNm/``````````````````````````````````````+sy/```..................."
+  sleep(0.05)
+  puts "```````````````````````````````````````````:+ydmso/oos+yyo/+o/:-+o:/:////+///://oymmy```````````````````````````````````````:yso`````.`..............."
+  sleep(0.05)
+  puts "````````````````````````````````````````````.:/so//+/////:--o:-.-/+---::--//:://syym:```````````````````````````````````````.ysy```````..............."
+  sleep(0.05)
+  puts "``````````````````````````````````````````````.++::/+::----//-..--://:--:::::://ssdy`````````````````````````````````````````yoh.``````````.`........."
+  sleep(0.05)
+  puts "```````````````````````````````````````````````-::-////:::/::--..--:-------::///dmh-`````````````````````````````````````````osh:`````````````........"
+  sleep(0.05)
+  puts "````````````````````````````````````````````````.:.:::---------..-:::-----::://+MN+.`````````````````````````````````````````/yy+``````````````......."
+  sleep(0.05)
+  puts "`````````````````````````````````````````````````.--/::-----+o+/+oo+:---:::://+sdo.``````````````````````````````````````````-yys``````````````......."
+  sleep(0.05)
+  puts "```````````````````````````````````````````````````.//:::::-::////--:://:::/+++/.````````````````````````````````````````````.hyy``````````````......."
+  sleep(0.05)
+  puts "````````````````````````````````````````````````````-///:://///////++/::::/+ooo:``````````````````````````````````````````````yyh.`````````````.````.."
+  sleep(0.05)
+  puts "`````````````````````````````````````````````````````-/+/::://////:::::::/+osso/.`````````````````````````````````````````````oyy:```````````````````."
+  sleep(0.05)
+  puts "```````````````````````````````````````````````````````:+//::::::------:/+oyso//+:````````````````````````````````````````````+hyo```````````````````."
+  sleep(0.05)
+  puts "````````````````````````````````````````````````````````.++/::--------:/osyo+//::..```````````````````````````````````````````:dyy```````````````````."
+  sleep(0.05)
+  puts "````````````````````````````````````````````````````````.+sso//::::://osys+/::-.``..++..--:++/.```````````````````````````````.dyd````````````````````"
+  sleep(0.05)
+  puts "``````````````````````````````````````````````````//-:+y::/osyssssssyyyyo+:-.````.`.-hdddmNNMNmy/.`````````````````````````````hhh-```````````````````"
+  sleep(0.05)
+  puts "``````````````````````````````````````````````` .omNmNNh----:+syyyyyyys+:-.````````..:NNmNNNNNNNmh+````````````````````````````shh/```````````````````"
+  sleep(0.05)
+  puts "``````````````````````````````````````````````  :mNNNNN+-.....-/ooo++/-.``````  ``...ymmNNNNNNmmmmm+.``````````````````````````+dyo```````````````````"
+  sleep(0.05)
+  puts "````````````````````````````````````````````/yyyhNNNmNm:.-.```.so++o+`````   ``...`.smmmNNNNNNNNNNmmdy/.```````````````````````:dyy```````````````````"
+  sleep(0.05)
+  puts "```````````````````````````````````````` `-ymmNNNNNNNmm:`...`.ydyo+/+/`   ````.```.ommmmNNNNNNNNmmmmmmmh/.`````````````````````.dyh```````````````````"
+  sleep(0.05)
+  puts "``````````````````````````````````` `` `:sdmmmNNNNNNNNm/````-+:hhyysoy/` ```````.:smmNNNmmNNNNNNNNNNNNNNmdyo:.``````````````````hyh-``````````````````"
+  sleep(0.05)
+  puts "````````````````````````````````` ` `-+ymmmmmNNNNNNNNNm+```-:..oohmdo:-.````` .:osdmNNNNNNNNNNNNNNNNNNmNmmmmNdo-````````````````shh/``````````````````"
+  sleep(0.05)
+  puts "```````````````````````````````  ``:smNNNNNNNNMNNNNNNNNo`.::``+yosh+```.`````:osydmmNNNNNNNmmmmmmmNmmmmmmmmmNNNNh/``````````````+hyo``````````````````"
+  sleep(0.05)
+  puts "```````````````````````````````./shmNNmmmNNNNNNNNNNNNNNy/s/.`:ho++oo````.``-osyssmNmNNNNNmmmmmmmmmmNNmmmmmmmNNNNNNh-````````````-hyy``````````````````"
+  sleep(0.05)
+  puts "`````````````````````````````:sdmNNNNmmmmNNNNNNNNNNNNNNmhyso:hso+oyds.`.--/yyssymNNNNNNmmNmNmmmmmmmmNNmmmmNmNNNNNNNm+```````````.hyd``````````````````"
+  sleep(0.05)
+  puts "```````````````````````````:ymNNNNNNNmmmNNNNNNNNNNNNNNNNysyyysssyhhhho/osyyssyydmNNNNNNNNNmmmmmmNNNNNNNNNNNmNNNNNNNNNs-``````````yyd.`````````````````"
+  sleep(0.05)
+  puts "`````````````````````````-yNNNNNNNNNNmmNNNNNNNNNNNNNNNNNdyssyhysyyhsssyysssyssyNNNNNNNNNNNNNNNmmNNNNNNNNNNNNNNNNNNNNMNm+`````````syd/`````````````````"
+  sleep(0.05)
+  puts "````````````````````````/mNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNmsyyyssyyssyysssysssssmNNNNNNNNNNNNmNmNNNNNNNNNNNNNNMNNNNNMNMMNNy.`````.-yyso.....````````````"
+  sleep(0.05)
+  puts "```````````````````````+NMMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNyssyyysosyssysssoossdNNMNNNNNNNNNNNNNNNNNNNNNNNNNNNMNNNNNMNMMMNNh-``-::/ys:...--:.```````````"
+  sleep(0.05)
+  puts "``````````````````````/mMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNdosyyssyhyyssoooooshNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNMMNNNNNMNNMMMNNd-.++++/---:+os+````````````"
+  sleep(0.05)
+  puts "```````````````````` -mNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNMNNNssosssssosooooooshNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNMMNNNNMMNNMMMMNNm/++o/--/oo+/+:````````````"
+  sleep(0.05)
+  puts "``````````````````` `hNMNNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNMNNysssssssossooooshNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNMMMNNNNNMNNNMMMMMm/::/o/o/-...-::-.`````````"
+  sleep(0.05)
+  puts "`````````````````   oNNMNNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNdsssssooosssoosdNNNNmNNNNNNNNNNNNMMMNhymMNNNNNNNMMNNNNNMMNNNMMMMMy--://-...--:+++/:`````````"
+  sleep(0.05)
+  puts "```````````````````:mNNMNNNMNNNNMNNNNNNNNNNNNNNNNNNNNNNNNNNhsssssoooooossNMMNNNNNNNNNNMNNhyydmyooyNNNNMNNMMMNNNNNMMNNNNNNNNo---//---:/+++/:-:.````````"
+  sleep(0.05)
+  puts "``````````````````-dmNMMMNNNNNMMMMMNNNNNNNNNNNNNNNNNNNMMMNNmysssssossooosNNNNNNNNNNNNNMNyo+//oo+sho//oNMMMMMNNNNNMMNNNNNNNN+---/so/:--.-::::-:.```````"
+  sleep(0.05)
+  puts "`````````````````+mmNNMMNNNNNNNMMMMMMNNNNNNNNNNNNNNNNNMMMMNdyssssoosooosyNNNNNNNNNNNNNMdysssoo+/ossosyyMMMMNNNNNNMNNNNNNNMM+--:/:-----:/+++:--.```````"
+  sleep(0.05)
+  puts "````````````````yNNNNNNMNNNNNNNNNMMMMMNNNNNNNNNNNNNNNNNNNNNdssssssoosshdmNNNNmNNNNNNNMMNhysooo++/:-oyhyNMMMNNNNNNMNNNNNMMMM+:-:o/::///:::-----:```````"
+  sleep(0.05)
+  puts "``````````````.hNNNNNNNMNNMNNNNNNMMMMMMNNNNNNNNNNNNNNNNNNNNmsosssooosymhdNMMMmNNMNNNNMNNNms+ooooo/:oymmMMMMNNNNNNNNMMMMMMMMh///sss:---::::::://```````"
+  sleep(0.05)
+  puts "`````````````:dNNNNNMNNMMNMMMNNNNMMMMMMMNNNNNNNNNNNNNMNMMMNmsssoosssosyhNNNNNNNNNNNNNMMMmhy/::/os+oodmMMMMNNNNNNNNNMMMMMMMMmh+/+osoosyhhyo+/+:.```````"
+  sleep(0.05)
+  puts "````````````+mmNNNNNNNNNMMNMMNNNNMMMMMMMMNNNNNNNNNNNNNNNMNNmssssssooososNNNNNNNNNNNNNMMMNdho-//ososhdNMMMMNNNNNNNMMMMNNNMMMdss+//+shhyyo/::+:`````````"
+  sleep(0.05)
+  puts "```````````smmmNNNNNNMNNMNMMMMNNNNMMMMMMMMMNNNNNNmdhdmNNNNNmosooooosoossNNNNNNNNNNNNNMMMMhsy/.:osshhNMMMMMMNNMNNMMMNNNNMMMMNsoo+/:::::::://-``````````"
+  sleep(0.05)
+  puts "`````````.hmmmNNNNMMNNMNMNNMNMMNNNMMMMMMMMMMNNNmdh+``.:+shmmssssoooooosyNNNNNNNNNNNNNNNMMdosdy+shhssmMMMMMMNMNNMMMNNNNMMMMMNm:/oo+/////+oo:```````````"
+  sleep(0.05)
+  puts "````````:dmmNNNNNNNNNNNMNMNNNNMMMNMMMMMMMMMMNmdhhh+```````/hyssssoosssshNNNNNNNNNNNNNNNNNMmdhysyyyhdMMMMMMNMNNMMMNNNNNMMMMMNMo:-+oooooss+os```````````"
+  sleep(0.05)
+  puts "``````yNMMMMMMMNNNNNNNNNMMMNNNMMMMMMMMMMNmmdhhhydh+```````.ydhsoooossssmNNNNNNNNNNNNNNNNNNNNMMNNNNNNNNMMMMMNNMMNNMNNNNMMMMNNMMhyyoooooyyyh+```````````"
+end
